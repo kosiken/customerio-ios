513b3b5d00b56e558a0902e04d931b9f1f69f94d
Levi Bostian <levi@customer.io>
2024-04-23 13:49:08 -0500

    POC performance of a List of inline messages
    This commit shows a list of 10 rows where each row contains 1 inline in-app message. Scroll the list up and down to see how responsive the UI is as you scroll.
    
    Note: When you first open the app, the rows of the table are the incorrect height. If you scroll down the list and then back up, the cell heights will get corrected. This issue is probably a sample app List issue and not an issue with the inline in-app message. It's been a while since I've created a UIKit TableView 😅.

--- Apps/APN-UIKit/APN UIKit/ViewController.swift
+++ Apps/APN-UIKit/APN UIKit/ViewController.swift
@@ -5,13 +5,35 @@ import UIKit
  Very simple screen in the app where we can display inline in-app messages, like a customer would.
  */
 class POCController: UIViewController {
-    @IBOutlet var gistView: GistView!
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Customers would not actually call this function.
         // Calling it here to trigger the View to begin displaying it's content.
+//        gistView.startItUp()
+
+        tableView.dataSource = self
+    }
+}
+
+extension POCController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GistCell", for: indexPath) as! GistTableViewCell
+        cell.setup()
+        return cell
+    }
+}
+
+class GistTableViewCell: UITableViewCell {
+    @IBOutlet var gistView: GistView!
+
+    func setup() {
         gistView.startItUp()
     }
 }
